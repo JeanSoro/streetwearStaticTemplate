3f8ebf562cbbac9e4c3b9732d25a2808612fce1a
Jean <jean.soro007@gmail.com>
2020-07-17 11:38:53 -0400

    Loaded gsap animation scripts

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,21 @@
+
+let tl = gsap.timeline();
+
+tl.fromTo(
+  "#jumbo .jumbo-titles h1",
+  {
+    x: -100,
+    opacity: 0
+  },
+
+  {
+    x: 0,
+    opacity: 1,
+    duration: .5
+  }
+)
+
+
 $(window).on("scroll", function () {
   if ($(window).scrollTop() > 700) {
     $(".header").addClass("active");
