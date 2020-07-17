c22a3d0523c073ca0d5e66bee3f859edb6528ab5
Jean <jean.soro007@gmail.com>
2020-07-17 12:41:18 -0400

    Implemented animations for header and jumbo sections

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,114 @@
 
-let tl = gsap.timeline();
+let jumboTL = gsap.timeline();
 
-tl.fromTo(
-  "#jumbo .jumbo-titles h1",
+jumboTL.fromTo(
+  '#jumbo .transparent-color',
   {
-    x: -100,
-    opacity: 0
+    opacity: 1,
+    backgroundColor: "black"
   },
-
+  {
+    opacity: .8,
+    duration: 1.5
+  }
+).fromTo(
+  '.logo',
+  {
+    x: -200,
+    opacity: 0,
+  },
   {
     x: 0,
     opacity: 1,
-    duration: .5
+    duration: 1.2
   }
+).fromTo(
+  '.menu',
+  {
+    x: 200,
+    opacity: 0,
+  },
+  {
+    x: 0,
+    opacity: 1,
+    duration: 1.2
+  },
+  // offset after 1 animation is done header menu animations come in at the same time
+  '>-1.3'
+).fromTo(
+  '#jumbo .jumbo-titles h1',
+  {
+    x: 0,
+    opacity: 0,
+  },
+  {
+    x: 100,
+    opacity: 1,
+    duration: 1.2
+  },
+  // offset after 1 animation is done header menu animations come in at the same time
+  '>-.3'
+).fromTo(
+  '#jumbo .jumbo-titles h2',
+  {
+    x: 0,
+    opacity: 0,
+  },
+  {
+    x: 135,
+    opacity: 1,
+    duration: 1.2
+  },
+  // offset after 1 animation is done header menu animations come in at the same time
+  '>-.7'
+).fromTo(
+  '#jumbo .state',
+  {
+    x: "100%",
+  },
+  {
+    x: 0,
+    duration: 1
+  }
+  // offset after 1 animation is done header menu animations come in at the same time
+
+).fromTo(
+  '#jumbo .store-info',
+  {
+    y: "100%",
+  },
+  {
+    y: 0,
+    duration: 1
+  },
+  // offset after 1 animation is done header menu animations come in at the same time
+  '>-1.8'
+).fromTo(
+  '#jumbo .store-info img',
+  {
+    y: 400,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+    stagger: .2
+  },
+  // offset after 1 animation is done header menu animations come in at the same time
+  '>-1.8'
 )
+// jumboTL.fromTo(
+//   '#jumbo .jumbo-image',
+//   {
+//     opacity: 0
+//   },
+//   {
+//     opacity: 1,
+//     duration: 1.5
+//   }
+// )
+
 
 
 $(window).on("scroll", function () {
